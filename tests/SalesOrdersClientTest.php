707f328b07fbb6c58a92a5a48f2190e2db131be5
Corey Hickson <chickson@vendasta.com>
2020-07-23 11:06:10 -0600

    Cleanup namespacing
    GLO-1537

--- tests/SalesOrdersClientTest.php
+++ tests/SalesOrdersClientTest.php
@@ -3,11 +3,8 @@
 use PHPUnit\Framework\TestCase;
 use Salesorders\V1\CreateAndActivateOrderRequest;
 use Vendasta\SalesOrders\V1\SalesOrdersClient;
+use Vendasta\SalesOrders\V1\SalesOrdersUtils;
 use Vendasta\Vax\SDKException;
-use function Vendasta\SalesOrders\V1\buildGoDaddyCustomFields;
-use function Vendasta\SalesOrders\V1\buildGSuiteCustomField;
-use function Vendasta\SalesOrders\V1\buildLineItem;
-use function Vendasta\SalesOrders\V1\buildOrder;
 
 class SalesOrdersClientTest extends TestCase
 {
@@ -24,8 +21,8 @@ class SalesOrdersClientTest extends TestCase
         $req = new CreateAndActivateOrderRequest();
 
         // Create the line items
-        $gSuite = buildLineItem('MP-6XDHVMQ84K4THNNP2Z7W2GC28VLHRC4Q');
-        $goDaddy = buildLineItem('MP-NNTJMBF6HPXR5XXC2JKCFWKJ64VZLBFQ');
+        $gSuite = SalesOrdersUtils::buildLineItem('MP-6XDHVMQ84K4THNNP2Z7W2GC28VLHRC4Q');
+        $goDaddy = SalesOrdersUtils::buildLineItem('MP-NNTJMBF6HPXR5XXC2JKCFWKJ64VZLBFQ');
         $lineItems = array($gSuite, $goDaddy);
 
         // Create the custom field
